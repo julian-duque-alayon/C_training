9411556889e99b561251b57159cf1b9d457faf5c
Julian Duque <julian.duque.alayon@gmail.com>
2025-12-18 17:20:31 +0100

    First steps with threads

--- threads/src/main.c
+++ threads/src/main.c
@@ -1,9 +1,21 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void *foo(); 
+
+//Do I always need to create a prototype of my function?
+//difference between void* foo and void *foo ?
 
 int main(void) {
-    printf("Hola, C!\n");
-    printf("Plantilla lista para proyectos serios.\n");
-    
-    return EXIT_SUCCESS;
+    pthread_t thread1;
+    pthread_create(&thread1, NULL, foo, NULL);
+    pthread_join(thread1, NULL);
+    return 0;
+}
+
+void *foo(){
+    printf("Computation \n");
+    return NULL;
 }
